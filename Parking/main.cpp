95fd70a6987b5f4dc17ecb308d9755b0226d9a8f
fm1ck3y <arte.vdovin@gmail.com>
2020-10-17 14:58:22 +0300

    add update,delete and check on leak memory

--- Parking/main.cpp
+++ Parking/main.cpp
@@ -17,56 +17,57 @@ int main()
     Payment::Select();
     Car::Select();
 
-    // for (auto user : User::users)
-    // {
-    //     user->PrintInfo();
-    //     cout << endl
-    //          << endl;
-    // }
-    // cout << "-----------------------------------" << endl;
-    // for (auto car : Car::cars)
-    // {
-    //     car->PrintInfo();
-    //     cout << endl
-    //          << endl;
-    // }
-    // cout << "-----------------------------------" << endl;
-    // for (auto visit : Visit::visits)
-    // {
-    //     visit->PrintInfo();
-    //     cout << endl
-    //          << endl;
-    // }
-    // cout << "-----------------------------------" << endl;
-    // for (auto payment : Payment::payments)
-    // {
-    //     payment->PrintInfo();
-    //     cout << endl
-    //          << endl;
-    // }
+    for (auto user : User::users)
+    {
+        user->PrintInfo();
+        cout << endl
+             << endl;
+    }
+    cout << "-----------------------------------" << endl;
+    for (auto car : Car::cars)
+    {
+        car->PrintInfo();
+        cout << endl
+             << endl;
+    }
+    cout << "-----------------------------------" << endl;
+    for (auto visit : Visit::visits)
+    {
+        visit->PrintInfo();
+        cout << endl
+             << endl;
+    }
+    cout << "-----------------------------------" << endl;
+    for (auto payment : Payment::payments)
+    {
+        payment->PrintInfo();
+        cout << endl
+             << endl;
+    }
 
     // пример того, что при загрузке БД , новая память не выделяется
     User *user = *(User::users.begin());
     user->SetFIO("Софронов Лев Владимирович");
     user->Save();
     User::Select();
-    // cout << user->GetFIO() << endl;
+    cout << user->GetFIO() << endl;
     user->Delete();
-    // //User::Select();
-    //cout << "-----------------------------------" << endl;
-    // for (auto user : User::users)
-    // {
-    //     user->PrintInfo();
-    //     cout << endl
-    //          << endl;
-    // }
-    // cout << "-----------------------------------" << endl;
+    //User::Select();
+    cout << "-----------------------------------" << endl;
+    for (auto user : User::users)
+    {
+        user->PrintInfo();
+        cout << endl
+             << endl;
+    }
+    cout << "-----------------------------------" << endl;
     //user->Delete();
     //cout << user->GetFIO() << endl;
     for (auto user : User::users) delete user;
     for (auto payment : Payment::payments) delete payment;
     for (auto visit : Visit::visits) delete visit;
     for (auto car : Car::cars) delete car;
+    Visit::visits = nullptr;
     Database::Disconnect();
     //User *user = new User("Вдовин Артём Андреевич","Линина 71", "78005553535","22 15 333333");
     //user->Create();
