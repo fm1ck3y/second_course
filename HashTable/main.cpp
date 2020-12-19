4e0bffccdbbf8064c9e223b291778a7eac0eb614
fm1ck3y <arte.vdovin@gmail.com>
2020-12-20 02:06:54 +0300

    change main

--- HashTable/main.cpp
+++ HashTable/main.cpp
@@ -1,25 +1,19 @@
 #include "HashTable.cpp"
 #include <list>
 #include <iostream>
+
 int main(void)
 {
-    // std::list<HashInfo<int,int>> ** table = (std::list<HashInfo<int,int>> **)malloc(9 * sizeof(std::list<HashInfo<int,int>>));
-    // for(std::size_t i = 0 ; i < 9; i++)
-    //     table[i] = new std::list<HashInfo<int,int>>();
-    // std::list<HashInfo<int,int>> *_l = table[1];
-    // HashInfo<int,int> *hs = new HashInfo<int,int>(1,2);
-    // _l->push_back(*hs);
-    // for (auto el : *_l)
-    //    std::cout << el << std::endl;
-
     HashTable<std::string, int> *hashtab = new HashTable<std::string, int>(17);
     hashtab->Add("hello", 22);
     hashtab->Add("world", 333);
     hashtab->Add("kek", 999);
+    hashtab->Add("Hello world", -25);
     hashtab->Print();
-    std::cout << (*hashtab)["kek"] << std::endl;
-    std::cout << hashtab->IsExist("kek") << std::endl;
-    std::cout << hashtab->IsExist("l") << std::endl << std::endl;
+    std::cout << "Value by key: 'kek' => " << (*hashtab)["kek"] << std::endl;
+    std::cout << "Key 'kek' is exist =>'" << hashtab->IsExist("kek") << std::endl;
+    std::cout << "Key 'l' is exist =>'" << hashtab->IsExist("l") << std::endl
+              << std::endl;
     hashtab->Print();
     delete hashtab;
 }
